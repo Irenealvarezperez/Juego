1713ffdd677b308b6bdebbc56c3250d3c6254b9e
alejandrodelmoral <alejandro.moral.lapresta@alumnos.upm.es>
2021-05-08 12:25:24 +0200

    se incluye la librería ETSIDI

---
+++ Juego/src/Mundo.h
@@ -0,0 +1,15 @@
+#pragma once
+
+class Mundo
+{
+public:
+	void Tecla(unsigned char key);
+	void Inicializa();
+	void RotarOjo();
+	void Mueve();
+	void Dibuja();
+
+	float x_ojo;
+	float y_ojo;
+	float z_ojo;
+};
