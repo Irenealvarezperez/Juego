890c426675d56f09491a680c6ce16cd6656f1914
alejandrodelmoral <alejandro.moral.lapresta@alumnos.upm.es>
2021-05-13 20:05:45 +0200

    corregido disparos sonidos

--- Juego/src/ListaDisparos.cpp
+++ Juego/src/ListaDisparos.cpp
@@ -1,6 +1,5 @@
 #include "ListaDisparos.h"
 
-
 ListaDisparos::ListaDisparos() {
 	numero = 0;
 	for (int i = 0; i < MAX_DISPAROS; i++) {
@@ -11,31 +10,33 @@ ListaDisparos::ListaDisparos() {
 bool ListaDisparos::agregar(Disparo* d, float flip) {
 	if (numero < MAX_DISPAROS) {
 		lista[numero++] = d;
-		if (flip != 0) {
+		if (flip != 0)
 			d->sprite_disparo->setAngle(flip);
-			ETSIDI::play("sonidos/disparo.mp3");
-		}
+		ETSIDI::play("sonidos/disparo.mp3");
 	}
 	else return false;
 	return true;
 }
+
 void ListaDisparos::destruirContenido() {
 	for (int i = 0; i < numero; i++)
 		delete lista[i];
 	numero = 0;
 }
+
 void ListaDisparos::mueve(float t) {
 	for (int i = 0; i < numero; i++) {
 		lista[i]->mueve(t);
 	}
 }
+
 void ListaDisparos::dibuja() {
 	for (int i = 0; i < numero; i++) {
 		lista[i]->dibuja();
 		
 	}
 }
+
 ListaDisparos::~ListaDisparos() {
 	destruirContenido();
 }
-
