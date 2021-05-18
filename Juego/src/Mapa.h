d6384467daa3699ce92f11687a6dd274e4d4ccb2
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2021-05-18 18:20:38 +0200

    Se modifica la lectura de archivos

--- Juego/src/Mapa.h
+++ Juego/src/Mapa.h
@@ -3,6 +3,7 @@
 #include "ListaSuelos.h"
 #include "ListaEnemigos.h"
 #include "ListaBonus.h"
+#include <string>
 
 class Mapa
 {
@@ -11,6 +12,7 @@ private:
 	static const int columna_max = 80;
 	ListaSuelos suelos;
 	int pantalla = 1;
+	string str = "..\\src\\Nivel.txt";
 
 public:
 	void inicia(ListaBonus& bonus, ListaEnemigos &enemigos);
@@ -19,6 +21,8 @@ public:
 	const int getFila_Max() { return fila_max; };
 	const int getColumna_Max() { return columna_max; };
 	void setPantalla(int nivel) { pantalla = nivel; };
+	void crear();
+	
 	friend class Interaccion;
 	friend class ListaSuelos;
 	friend class Mundo;
