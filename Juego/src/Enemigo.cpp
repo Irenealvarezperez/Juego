2f7af2de4466356122231d94ff08da867a710b91
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2021-05-27 16:02:03 +0200

    Limpieza codigo que ya no se usa en la clase enemigo

--- Juego/src/Enemigo.cpp
+++ Juego/src/Enemigo.cpp
@@ -1,32 +1,12 @@
 #include "Enemigo.h"
-#include "freeglut.h"
-
-Enemigo::Enemigo()
-{
-	rojo = verde = azul = 255; //blanco por defecto
-	aceleracion.x = 3.0f;
-	aceleracion.y = -9.8f;
-	altura = ancho = 5.5f;
-	//enemigo = new Sprite("imagenes/enemigo.png", posicion.x, posicion.y, ancho, altura);
-	setVel(1, 1);
-}
 
 void Enemigo::dibuja()
 {
-	/*
-	glPushMatrix();
-	glTranslatef(posicion.x, posicion.y, 0);
-	glColor3f(rojo, verde, azul);
-	glutSolidCube(altura);
-	glPopMatrix();
-	*/
-	//sprite->draw();
 	sprite->draw();
 }
 
 void Enemigo::mueve(float t)
 {
-	//posicion = sprite->getPos();// Sobra, comprobado
 	posicion = posicion + velocidad * t + aceleracion * (0.5f * t * t);
 	velocidad = velocidad + aceleracion * t;
 
@@ -38,20 +18,10 @@ void Enemigo::mueve(float t)
 	disparos.mueve(t);
 }
 
-void Enemigo::setColor(unsigned char r, unsigned char v, unsigned char a)
-{
-	rojo = r;
-	verde = v;
-	azul = a;
-}
-
 Enemigo::Enemigo(float altura, float x, float y, float vx, float vy)
 {
 	this->altura = altura;
-	rojo = verde = 255;
-	azul = 100; //color distinto
-	aceleracion.x = 0;
-	aceleracion.y = 0;
+
 	// Aqui deberia dar igual lo q poner porq luego se modifica desde la clase especializada, por ej Ladron,
 	// pero si cambian el numero de filas o columnas de aqui || no se q pasa
 	//														 vv
--- Juego/src/Enemigo.h
+++ Juego/src/Enemigo.h
@@ -1,17 +1,12 @@
 #pragma once
 
 #include "ListaDisparos.h"
-#include"ETSIDI.h"
 #include "ObjetoMovil.h"
-using namespace ETSIDI;
 
 class Enemigo:public ObjetoMovil
 {
 protected:
 	int vida = 1;
-	unsigned char rojo;
-	unsigned char verde;
-	unsigned char azul;
 	float altura;
 	float ancho = 4;
 	ListaDisparos disparos;
@@ -19,14 +14,12 @@ protected:
 	Tipo tipo;
 
 public:
-	Enemigo();
 	Enemigo(float altura = 5.0, float x = 0.0, float y = 0.0, float vx = 0.0, float vy = 0.0);
 
 	void setPos(float ix, float iy) { posicion.x = ix, posicion.y = iy, sprite->setPos(ix, iy); }; //enemigo
 	void setVel(float vx, float vy) { velocidad.x = vx, velocidad.y = vy, sprite->setVel(vx, vy); } //enemigo
 	virtual void dibuja();
 	void mueve(float t);
-	void setColor(unsigned char r, unsigned char v, unsigned char a);
 	float getAncho() { return ancho; };
 	void setTipo(Tipo tipo) { this->tipo = tipo; };
 
