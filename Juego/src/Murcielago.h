de973919ef2420df00aea8a011e52a7ea03388ce
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2021-06-08 22:37:17 +0200

    Nuevos metodos para murcielago

--- Juego/src/Murcielago.h
+++ Juego/src/Murcielago.h
@@ -2,9 +2,16 @@
 #include"Enemigo.h"
 class Murcielago: public Enemigo
 {
+private:
+    long t0 = 0;
+    long t1;
 public:
     Murcielago(float altura, float anchura, float x, float y, float vx, float vy);
     void dibuja();
     void dispara(float vx, float vy, float flip);
+    long getTime0() { return t0; };
+    long getTime1() { return t1; };
+    void setTime0(long t0) { this->t0 = t0; };
+    void setTime1(long t1) { this->t1 = t1; };
 };
 
