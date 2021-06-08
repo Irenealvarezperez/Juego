57862ef7e57a8ec2100c41495b7c45ce6a0024ba
jacobsanchezmoreira <47833441+jacobsanchezmoreira@users.noreply.github.com>
2021-06-08 22:38:30 +0200

    Muerte de granvirus crea minivirus

--- Juego/src/GranVirus.cpp
+++ Juego/src/GranVirus.cpp
@@ -1,4 +1,5 @@
 #include "GranVirus.h"
+
 GranVirus::GranVirus(float altura, float anchura, float x, float y, float vx, float vy) :Enemigo(altura, anchura, x, y, vx, vy)
 {
 	setTipo(GRANVIRUS);
@@ -9,10 +10,17 @@ GranVirus::GranVirus(float altura, float anchura, float x, float y, float vx, fl
 
 void GranVirus::dibuja() {
 	Enemigo::dibuja();
+	disparos.dibuja();
 
 	if (vida == 3)BarradeVida->setState(0, false);
 	else if (vida == 2)BarradeVida->setState(3, false);
 	else if (vida == 1)BarradeVida->setState(6, false);
 	BarradeVida->setPos(posicion.x, posicion.y + altura / 2);
 	BarradeVida->draw();
+}
+
+void GranVirus::dispara(ListaEnemigos& enemigos)
+{
+	enemigos.agregar(new Minivirus(6, 6, posicion.x - 5, posicion.y - 3, -1, 0));
+	enemigos.agregar(new Minivirus(6, 6, posicion.x + 5, posicion.y - 3, -1, 0));
 }
--- Juego/src/GranVirus.h
+++ Juego/src/GranVirus.h
@@ -1,10 +1,13 @@
 #pragma once
 #include "Enemigo.h"
-class GranVirus :
-    public Enemigo
+#include "Minivirus.h"
+#include "ListaEnemigos.h"
+
+class GranVirus :public Enemigo
 {
 public:
     GranVirus(float altura,float anchura, float x, float y, float vx, float vy);
     void dibuja();
+    void dispara(ListaEnemigos& enemigos);
 };
 
